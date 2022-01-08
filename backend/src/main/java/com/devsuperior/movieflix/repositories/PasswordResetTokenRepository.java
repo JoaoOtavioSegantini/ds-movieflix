9ead48f060d58a33acf8bf1cc049e409f581225a
JoaoOtavioSegantini <71529922+JoaoOtavioSegantini@users.noreply.github.com>
2022-01-08 08:09:20 -0300

    Update PasswordResetTokenRepository.java

--- backend/src/main/java/com/devsuperior/movieflix/repositories/PasswordResetTokenRepository.java
+++ backend/src/main/java/com/devsuperior/movieflix/repositories/PasswordResetTokenRepository.java
@@ -16,13 +16,4 @@ public interface PasswordResetTokenRepository extends JpaRepository<PasswordRese
 
     PasswordResetToken findByToken(String token);
 
-    PasswordResetToken findByUser(User user);
-
-    Stream<PasswordResetToken> findAllByExpiryDateLessThan(Date now);
-
-    void deleteByExpiryDateLessThan(Date now);
-
-    @Modifying
-    @Query("delete from PasswordResetToken t where t.expiryDate <= ?1")
-    void deleteAllExpiredSince(Date now);
 }
