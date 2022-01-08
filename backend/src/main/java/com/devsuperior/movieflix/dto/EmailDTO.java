0728416e5f68bd84570df7821ff7a309be0e9cf1
JoaoOtavioSegantini <71529922+JoaoOtavioSegantini@users.noreply.github.com>
2022-01-08 08:19:14 -0300

    Update EmailDTO.java

--- backend/src/main/java/com/devsuperior/movieflix/dto/EmailDTO.java
+++ backend/src/main/java/com/devsuperior/movieflix/dto/EmailDTO.java
@@ -1,58 +1,35 @@
 package com.devsuperior.movieflix.dto;
 
 import java.io.Serializable;
+import java.util.Map;
 
-public class EmailDTO  implements Serializable {
-	
-	/**
-	 * 
-	 */
+public class EmailDTO implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	private String fromEmail;
-	private String fromName;
-	private String replyTo;
+
+	private String from;
 	private String to;
 	private String subject;
-	private String body;
-	private String contentType;
-	
-	public EmailDTO () {
-		
+	private Map<String, Object> model;
+
+	public EmailDTO() {
+
 	}
 
-	public EmailDTO(String fromEmail, String fromName, String replyTo, String to, String subject, String body,
-			String contentType) {
-		this.fromEmail = fromEmail;
-		this.fromName = fromName;
-		this.replyTo = replyTo;
+	public EmailDTO(String from, String to, String subject, Map<String, Object> model) {
+		super();
+		this.from = from;
 		this.to = to;
 		this.subject = subject;
-		this.body = body;
-		this.contentType = contentType;
+		this.model = model;
 	}
 
-	public String getFromEmail() {
-		return fromEmail;
+	public String getFrom() {
+		return from;
 	}
 
-	public void setFromEmail(String fromEmail) {
-		this.fromEmail = fromEmail;
-	}
-
-	public String getFromName() {
-		return fromName;
-	}
-
-	public void setFromName(String fromName) {
-		this.fromName = fromName;
-	}
-
-	public String getReplyTo() {
-		return replyTo;
-	}
-
-	public void setReplyTo(String replyTo) {
-		this.replyTo = replyTo;
+	public void setFrom(String from) {
+		this.from = from;
 	}
 
 	public String getTo() {
@@ -71,22 +48,13 @@ public class EmailDTO  implements Serializable {
 		this.subject = subject;
 	}
 
-	public String getBody() {
-		return body;
+	public Map<String, Object> getModel() {
+		return model;
 	}
 
-	public void setBody(String body) {
-		this.body = body;
+	public void setModel(Map<String, Object> model) {
+		this.model = model;
 	}
-
-	public String getContentType() {
-		return contentType;
-	}
-
-	public void setContentType(String contentType) {
-		this.contentType = contentType;
-	}
-	
-	
 
 }
+
