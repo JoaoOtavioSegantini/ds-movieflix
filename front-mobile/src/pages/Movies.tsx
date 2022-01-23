954b656eed697e4b6a36e819215e8be2cd7af27b
João Otávio <joaotavioos@outlook.com>
2022-01-23 12:05:55 -0300

    Add mobile main project

---
+++ front-mobile/src/pages/Movies.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const Movies: React.FC = () => {
+  return <View></View>;
+};
+
+export default Movies;
---
+++ front-mobile/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Movies, Home, MovieDetails } from "../pages";
+
+const Stack = createNativeStackNavigator();
+
+const Routes: React.FC = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Movies" component={Movies} />
+      <Stack.Screen name="MovieDetails" component={MovieDetails} />
+    </Stack.Navigator>
+  );
+};
+
+export default Routes;
