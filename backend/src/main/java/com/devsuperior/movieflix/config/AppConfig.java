fc7e33152d3625feb7a8b1178c9b4766cc8d9210
João Otávio <joaotavioos@outlook.com>
2023-05-21 13:19:49 -0300

    feat: add default token service for custom generator ✨

--- backend/src/main/java/com/devsuperior/movieflix/config/AppConfig.java
+++ backend/src/main/java/com/devsuperior/movieflix/config/AppConfig.java
@@ -1,24 +1,27 @@
 package com.devsuperior.movieflix.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
-	
-	
+
 	@Value("${jwt.secret}")
 	private String jwtSecret;
-	
+
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder( ) {
+	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
@@ -29,6 +32,21 @@ public class AppConfig {
 	@Bean
 	public JwtTokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		defaultTokenServices.setSupportRefreshToken(true);
+		defaultTokenServices.setTokenEnhancer(enhancerChain());
+		return defaultTokenServices;
+	}
+
+	TokenEnhancerChain enhancerChain() {
+		TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+		enhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConverter()));
+		return enhancerChain;
 	}
 
 }
